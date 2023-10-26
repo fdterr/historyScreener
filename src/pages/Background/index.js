57880df25350a84f7c7aaae95686c90e735f28cd
Rick Terry <rterry@nymets.com>
2023-10-25 20:14:28 -0400

    remove background page, separate modal into separate component

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,24 +1,49 @@
 console.log("This is the background page.");
 
+let sites = [];
+
 chrome.runtime.onMessage.addListener(async function (
   request,
   sender,
   sendResponse
 ) {
-  if (request.type === "mount") {
-    console.log(
-      "Put the background scripts here.",
-      await chrome.history.search({ text: "", startTime: 0, maxResults: 0 })
-    );
+  const sites = request.sites;
+  console.log("sites", sites.length);
+  if (!!sites.length) {
+    sites.forEach(async (site) => {
+      const parsedSite = parseSite(site);
+      console.log("parsedSite", parsedSite);
+      await deleteSiteFromHistory(parsedSite);
+    });
   }
+});
+
+// Parses a usable site from user input by removing any
+// preceeding http://, http://, and www. from the url
+function parseSite(site) {
+  const parsedSite = site
+    .replace("http://", "")
+    .replace("https://", "")
+    .replace("www.", "");
+  return parsedSite;
+}
+
+// Completely deletes a site from history
+// Deletes every history item whose url contains the site
+async function deleteSiteFromHistory(site) {
+  console.log("deleting", site);
   const history = await chrome.history.search({
     text: "",
     startTime: 0,
     maxResults: 0,
   });
-  const matches = history.filter((item) => item.url.includes("espn.com"));
+  const matches = history.filter((item) => item.url.includes(site));
   console.log("matches", matches);
   matches.forEach((match) => chrome.history.deleteUrl({ url: match.url }));
-});
+}
+
+setInterval(() => {
+  console.log("time", new Date());
+}, 5000);
 
 (async () => {})();
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -3,18 +3,57 @@ import { Button, Input, Modal } from "@mantine/core";
 
 import "./Popup.css";
 
+const ConfirmModal = ({
+  deleteConfirm,
+  setDeleteConfirm,
+  setDeleteSite,
+  setSites,
+  deleteSite,
+  sites,
+}) => (
+  <Modal
+    opened={deleteConfirm}
+    onClose={setDeleteConfirm}
+    closeOnClickOutside={false}
+    title="Authentication"
+  >
+    <p>Are you sure you want to delete {deleteSite}</p>{" "}
+    <Button
+      onClick={() => {
+        console.log("deleting", deleteSite);
+        console.log('sites', sites);
+        console.log(
+          "newSites is",
+          sites.filter((s) => s !== deleteSite)
+        );
+        setSites(sites?.filter((s) => s !== deleteSite));
+        setDeleteConfirm(false);
+        setDeleteSite(null);
+      }}
+    >
+      Yes
+    </Button>
+  </Modal>
+);
+
 const Popup = () => {
   const [sites, setSites] = useState([]);
   const [newSite, setNewSite] = useState("");
   const [deleteConfirm, setDeleteConfirm] = useState(false);
   const [deleteSite, setDeleteSite] = useState(null);
 
+  // On mount (Popup opened), get blocked sites from local storage
+  // If we have blocked sites, set them in state and send to background page
   useEffect(() => {
-    chrome.runtime.sendMessage({ type: "mount" });
-
     const blockedSites = localStorage.getItem("history_blocked_sites");
+
     if (blockedSites) {
       setSites(JSON.parse(blockedSites));
+
+      chrome.runtime.sendMessage({
+        type: "mount",
+        sites: JSON.parse(blockedSites),
+      });
     }
   }, []);
 
@@ -28,60 +67,49 @@ const Popup = () => {
 
   return (
     <div className="App">
-      <table>
-        <tbody>
-          {sites.map((site, index) => (
-            <tr key={index}>
-              <td>{site}</td>
-              <td>
-                <button
-                  onClick={() => {
-                    // setSites(sites.filter((s) => s !== site));
-                    setDeleteConfirm(true);
-                    setDeleteSite(site);
-                  }}
-                >
-                  Remove
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div style={{ display: "flex" }}>
-        <Input
-          value={newSite}
-          onChange={(evt) => setNewSite(evt.target.value)}
-          placeholder="Add a site"
-        />{" "}
-        <button
-          onClick={() => {
-            setSites([...sites, newSite]);
-            setNewSite("");
-          }}
-        >
-          Add
-        </button>
-        {/* <div id="delete-confirm" hidden={!deleteConfirm}> */}
-        {/* Are you sure you want to delete {deleteSite} */}
-        {/* </div> */}
-        <Modal
-          opened={deleteConfirm}
-          onClose={setDeleteConfirm}
-          closeOnOutsideClick={false}
-          title="Authentication"
-        >
-          <p>Are you sure you want to delete {deleteSite}</p>{" "}
-          <Button
+      <div className="InnerApp">
+        <table>
+          <tbody>
+            {sites?.map((site, index) => (
+              <tr key={index}>
+                <td>{site}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      setDeleteConfirm(true);
+                      setDeleteSite(site);
+                    }}
+                  >
+                    Remove
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div style={{ display: "flex" }}>
+          <Input
+            value={newSite}
+            onChange={(evt) => setNewSite(evt.target.value)}
+            placeholder="Add a site"
+          />{" "}
+          <button
             onClick={() => {
-              setSites(sites.filter((s) => s !== deleteSite))
-              setDeleteConfirm(false);
-              setDeleteSite(null);
+              setSites([...sites, newSite]);
+              setNewSite("");
             }}
           >
-            Yes
-          </Button>
-        </Modal>
+            Add
+          </button>
+          <ConfirmModal
+            deleteConfirm={deleteConfirm}
+            setDeleteConfirm={setDeleteConfirm}
+            deleteSite={deleteSite}
+            setSites={setSites}
+            setDeleteSite={setDeleteSite}
+            sites={sites}
+          />
+        </div>
       </div>
     </div>
   );
