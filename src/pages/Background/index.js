219781e10dd3dc88301a4838d44f89bfc86bb46f
Rick Terry <rterry@nymets.com>
2023-10-28 22:33:44 -0400

    store sites in storage in background page

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -2,12 +2,14 @@ console.log("This is the background page.");
 
 let sites = [];
 
+// When a new site is visited, check if it is in the list of blocked sites
+// and delete if so
 chrome.history.onVisited.addListener(async function (historyItem) {
   const parsedSite = parseSite(historyItem.url);
   const domainOnly = parsedSite.split("/")[0];
-  if (sites.includes(domainOnly)) {
+  if (sites.map((site) => site.url).includes(domainOnly)) {
     console.log("blocking", domainOnly);
-    await deleteSiteFromHistory(domainOnly);
+    chrome.history.deleteUrl({ url: historyItem.url });
   }
 });
 
@@ -17,14 +19,25 @@ chrome.runtime.onMessage.addListener(async function (
   sendResponse
 ) {
   if (request.type === "new_site") {
+    // On receiving a new site, parse the url, add it to the list of sites,
+    // and delete it from history
     console.log("received newSite");
-    const parsedSite = parseSite(request.site);
-    sites.push(parsedSite);
-    await deleteSiteFromHistory(parsedSite);
+    const parsedSite = parseSite(request.site.url);
+    sites.push({ ...request.site, url: parsedSite });
+    await writeSitesToStorage();
+    await deleteSiteFromHistory(parsedSite.url);
   }
 
   if (request.type === "blocked_sites") {
-    sites = request.sites.map(parseSite);
+    // When user opens the popup, receive the new list of blocked sites
+    // and set sites locally
+    const newSites = request.sites.map((site) => {
+      const parsedSite = parseSite(site.url);
+      return { ...site, url: parsedSite };
+    });
+
+    sites = newSites;
+    await writeSitesToStorage();
     console.log("received full sites list - parsed:", sites);
   }
 });
@@ -32,6 +45,7 @@ chrome.runtime.onMessage.addListener(async function (
 // Parses a usable site from user input by removing any
 // preceeding http://, http://, and www. from the url
 function parseSite(site) {
+  console.log("parsing", site);
   const parsedSite = site
     .replace("http://", "")
     .replace("https://", "")
@@ -52,4 +66,13 @@ async function deleteSiteFromHistory(site) {
   matches.forEach((match) => chrome.history.deleteUrl({ url: match.url }));
 }
 
-(async () => {})();
+// Write a list of sites to locaStorage
+async function writeSitesToStorage() {
+  await chrome.storage.local.set({ history_blocked_sites: sites });
+}
+
+(async () => {
+  // In case the user does not open the popup window after restarting chrome
+  // we must retrieve our list of blocked sites from localStorage
+  sites = await chrome.storage.local.get("history_blocked_sites");
+})();
