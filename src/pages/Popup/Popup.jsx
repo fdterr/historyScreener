5db2b73ebc3d25a397f6332d52f7c3ef728cc410
Rick Terry <rterry@nymets.com>
2023-11-25 00:43:28 -0500

    flagsmith feature flags

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,27 +1,37 @@
 import React from "react";
 import { Tabs } from "@mantine/core";
+import { useFlags } from "flagsmith/react";
 
 import URLs from "./URLs";
 
 import "./Popup.css";
 
 const Popup = () => {
-  return (
-    <div className="App">
-      <Tabs defaultValue="urls">
-        <Tabs.List grow>
-          <Tabs.Tab value="urls">URLs</Tabs.Tab>
-          <Tabs.Tab value="keywords">Keywords</Tabs.Tab>
-        </Tabs.List>
-        <Tabs.Panel value="urls">
-          <URLs />
-        </Tabs.Panel>
-        <Tabs.Panel value="keywords">
-          <div>Keywords here</div>
-        </Tabs.Panel>
-      </Tabs>
-    </div>
-  );
+  const tabsFlag = useFlags("history_sweeper_tabs");
+  if (tabsFlag.history_sweeper_tabs.enabled) {
+    return (
+      <div className="App">
+        <Tabs defaultValue="urls">
+          <Tabs.List grow>
+            <Tabs.Tab value="urls">URLs</Tabs.Tab>
+            <Tabs.Tab value="keywords">Keywords</Tabs.Tab>
+          </Tabs.List>
+          <Tabs.Panel value="urls">
+            <URLs />
+          </Tabs.Panel>
+          <Tabs.Panel value="keywords">
+            <div>Keywords here</div>
+          </Tabs.Panel>
+        </Tabs>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <URLs />
+      </div>
+    );
+  }
 };
 
 export default Popup;
